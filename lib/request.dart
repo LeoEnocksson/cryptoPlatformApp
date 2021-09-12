ec2a20149aa54af599d7c7730c07817e527ac146
Leo Enocksson <Leo.enocksson@essiq.se>
2021-09-12 15:26:44 +0200

    todo comment

--- lib/request.dart
+++ lib/request.dart
@@ -5,7 +5,9 @@ import 'package:http/http.dart' as http;
 //ip4: - 192.168.1.84
 //fake: - http://10.0.2.2:5000/
 //real: - http://127.0.0.1:5000/
+
 class RequestHandler {
+  //Connect to pyCharm server and return the data.
   static Future getData() async {
     String url = 'http://127.0.0.1:5000/';
     final response = await http.get(Uri.parse(url), headers: {
@@ -20,8 +22,12 @@ class RequestHandler {
     return response.body;
   }
 
+//Connecting to server - code taken from postman
   static testData() async {
-    var request = http.Request('GET', Uri.parse('http://127.0.0.1:5000/'));
+    var request = http.Request(
+        'GET',
+        Uri.parse(
+            'http://127.0.0.1:5000/')); //TODO: Något krånglar med uri/url:en. Kanske behöver specifiera ip adress.
 
     http.StreamedResponse response = await request.send();
 
